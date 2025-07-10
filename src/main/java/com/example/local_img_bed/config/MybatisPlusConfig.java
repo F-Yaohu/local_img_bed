d68c953ff3da7f138dc486290e18a3ea439798d5
Zhao Yang <3319269122@qq.com>
2025-07-10 19:56:41 +0800

    继续解决 flyway 和 mybatis-plus 之间的冲突，重新配置

--- src/main/java/com/example/local_img_bed/config/MybatisPlusConfig.java
+++ src/main/java/com/example/local_img_bed/config/MybatisPlusConfig.java
@@ -1,52 +1,17 @@
 package com.example.local_img_bed.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import javax.sql.DataSource;
 
 @Configuration
 public class MybatisPlusConfig {
-
     @Bean
-    public PaginationInnerInterceptor paginationInnerInterceptor() {
+    public PaginationInnerInterceptor PaginationInnerInterceptor() {
         PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
         paginationInnerInterceptor.setDbType(DbType.MYSQL);
         paginationInnerInterceptor.setOverflow(true);
-        return paginationInnerInterceptor;
-    }
-
-    @Bean
-    public MybatisPlusInterceptor mybatisPlusInterceptor() {
-        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(paginationInnerInterceptor());
-        return interceptor;
-    }
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        MybatisSqlSessionFactoryBean factoryBean = new MybatisSqlSessionFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:/mapper/*.xml"));
-        factoryBean.setTypeAliasesPackage("com.example.local_img_bed.entity");
-        factoryBean.setPlugins(mybatisPlusInterceptor());
-        return factoryBean.getObject();
-    }
-
-    @Bean
-    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-        return new SqlSessionTemplate(sqlSessionFactory);
-    }
-
-    @Bean
-    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
+        return  paginationInnerInterceptor;
     }
 }
